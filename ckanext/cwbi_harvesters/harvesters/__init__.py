2cccd135e8ef8680171e462052d71b8328fa31c7
bmulcahy <brian.p.mulcahy@erdc.dren.mil>
2026-03-23 11:29:16 -0400

    meh

--- ckanext/cwbi_harvesters/harvesters/__init__.py
+++ ckanext/cwbi_harvesters/harvesters/__init__.py
@@ -6,10 +6,10 @@ except ImportError:
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
 
-from ckanext.cwbi_harvesters.harvesters.base import CWBIGovHarvester  # NOQA F401
-from ckanext.cwbi_harvesters.harvesters.base import CWBIGovCSWHarvester  # NOQA F401
-from ckanext.cwbi_harvesters.harvesters.base import CWBIGovWAFHarvester  # NOQA F401
-from ckanext.cwbi_harvesters.harvesters.base import CWBIGovDocHarvester  # NOQA F401
+from ckanext.cwbi_harvesters.harvesters.base import CWBIHarvester  # NOQA F401
+from ckanext.cwbi_harvesters.harvesters.base import CWBICSWHarvester  # NOQA F401
+from ckanext.cwbi_harvesters.harvesters.base import CWBIWAFHarvester  # NOQA F401
+from ckanext.cwbi_harvesters.harvesters.base import CWBIDocHarvester  # NOQA F401
 from ckanext.cwbi_harvesters.harvesters.base import CWBIGeoportalHarvester  # NOQA F401
 from ckanext.cwbi_harvesters.harvesters.waf_collection import WAFCollectionHarvester  # NOQA F401
 from ckanext.cwbi_harvesters.harvesters.arcgis import ArcGISHarvester  # NOQA F401
